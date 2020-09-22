d0002d61f071453f3d4d90db82206384dc2b0dd5
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2020-09-22 13:38:16 +0100

    Added a soon™ message

--- src/components/Usage/Usage.js
+++ src/components/Usage/Usage.js
@@ -10,6 +10,9 @@ const Usage = () => (
     <p>
       <b>{getString('USAGE_NOTE_LABEL')}:</b> {getString('USAGE_NOTE_TEXT')} <code>.jpg</code>.
     </p>
+    <p>
+      There will be &#x1F434; here soon&#8482;
+    </p>
   </div>
 )
 
